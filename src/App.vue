a7a5c1f42d4bd0453818e0a7fb1672026f6a0b1b
Amelia Schulz <amelia.schulz@gmail.com>
2020-08-27 17:32:11 -0400

    Record user audio and display audio clip.

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,86 @@
 <template>
   <div id="app">
-    <h1>Vue photos</h1>
-    <div v-bind:key="photo.id" v-for="photo in photos">
-      <img :src="photo.url" />
+    <h1>Vue clips</h1>
+    <div>
+      <button ref="record">Record</button>
+      <button ref="stop">Stop</button>
     </div>
-    <audio autoplay></audio>
+    <section ref="sound-clips">
+      <div v-bind:key="clip.name" v-for="clip in clips">
+        <article>
+          <audio controls>
+            <source :src="clip.url" type="audio/ogg" />
+          </audio>
+          <p>{{clip.name}}</p>
+        </article>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-
-const constraints = {
-  audio: true,
-}
-
-const chunks = []
-
-const audio = document.querySelector('audio')
-
-navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-  audio.srcObject = stream
-})
-
 export default {
   name: 'App',
   components: {},
   data() {
     return {
-      photos: [],
+      clips: [],
     }
   },
   created() {
-    axios
-      .get('https://jsonplaceholder.typicode.com/photos?_limit=10')
-      .then((res) => (this.photos = res.data))
-      .catch((err) => console.log(err))
+    if (navigator.mediaDevices) {
+      const constraints = {
+        audio: true,
+      }
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        // success callback
+        .then((stream) => {
+          const mediaRecorder = new MediaRecorder(stream)
+          const record = this.$refs.record
+          const stop = this.$refs.stop
+
+          record.onclick = () => {
+            mediaRecorder.start()
+            console.log(mediaRecorder.state)
+            console.log('recorder started')
+            record.style.background = 'red'
+            record.style.color = 'black'
+          }
+
+          let chunks = []
+          console.log(chunks)
+          mediaRecorder.ondataavailable = (e) => {
+            chunks.push(e.data)
+          }
+
+          stop.onclick = () => {
+            mediaRecorder.stop()
+            console.log(mediaRecorder.state)
+            console.log('recorder stopped')
+            record.style.background = ''
+            record.style.color = ''
+          }
+
+          mediaRecorder.onstop = () => {
+            console.log('recorder stopped')
+
+            let clipName = prompt('Enter a name for your sound clip')
+
+            const blob = new Blob(chunks, { type: 'audio/ogg; codecs=opus' })
+            chunks = []
+
+            const audioURL = window.URL.createObjectURL(blob)
+
+            this.clips = [...this.clips, { name: clipName, url: audioURL }]
+          }
+        })
+
+        // error callback
+        .catch((err) => console.log(err))
+    } else {
+      console.log('getUserMedia is not supported in your browser')
+    }
   },
 }
 </script>
